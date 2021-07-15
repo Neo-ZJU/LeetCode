3589b07806aff26f7a055bee6c477d94d9ec6d57
Neo-ZJU <3160105027@zju.edu.cn>
2021-07-15 23:25:09 +0800

    learn jianzhio3

---
+++ src/JianZhi03.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class JianZhi03 {
+    public static void main(String[] args) {
+        int abcabcbb = new Solution03().lengthOfLongestSubstring("abcabcbb");
+        System.out.println(abcabcbb);
+    }
+}
+class Solution03 {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int front = 0;
+        int back = 0;
+        HashSet<Character> set = new HashSet<>();
+        int len = s.length();
+
+        while(front < len){
+            if(!set.contains(s.charAt(front))){
+                max = Math.max(front - back + 1, max);
+                set.add(s.charAt(front));
+            }else{
+                while(!set.isEmpty() && set.contains(s.charAt(front))){
+                    set.remove(s.charAt(back++));
+                }
+                back++;
+            }
+            front++;
+        }
+        return max;
+    }
+}
