30deee55f68d10ef9d2c620840b731db6cc53136
Neo-ZJU <3160105027@zju.edu.cn>
2021-07-19 22:36:49 +0800

    LeetCode1838

---
+++ src/LeetCode1838.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class LeetCode1838 {
+}
+class Solution1838 {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int max = 0;
+        int tempSum = 0;
+        int left = 0;
+        for(int i = 0; i < nums.length; i++){
+            while(nums[i] * (i - left) - tempSum > k){
+                tempSum -= nums[left++];
+            }
+            tempSum += nums[i];
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+}
