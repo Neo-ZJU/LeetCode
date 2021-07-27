e7e7e6b3cfe60fbc339030641ea16276e902ee55
Neo-ZJU <3160105027@zju.edu.cn>
2021-07-27 23:17:30 +0800

    LeetCode671

---
+++ src/LeetCode671.java
@@ -0,0 +1,38 @@
+public class LeetCode671 {
+}
+
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+ TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+
+class Solution671 {
+    int val = Integer.MAX_VALUE;
+    boolean isFind = false;
+    public int findSecondMinimumValue(TreeNode root) {
+        if(root == null) return -1;
+        int rootVal = root.val;
+        if(root.left == null || root.right == null) return -1;
+        dfs(root, rootVal);
+        if(!isFind) return -1;
+        return val;
+    }
+    public void dfs(TreeNode root, int rootVal){
+        if(root == null) return;
+        if(root.val > rootVal) {
+            isFind = true;
+            val = Math.min(val, root.val);}
+        System.out.println(val);
+        dfs(root.left, rootVal);
+        dfs(root.right, rootVal);
+    }
+}
